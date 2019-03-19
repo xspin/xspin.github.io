8ab347322dde76429cca1845cc102e42c9fcb71c
xspin <cherlmex@gmail.com>
2019-03-19 20:59:40 +0800

    update

--- js/particles.js
+++ js/particles.js
@@ -1,3 +1,14 @@
+if ( !window.requestAnimationFrame ) {
+	window.requestAnimationFrame = ( function() {
+		return window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+			window.setTimeout( callback, 1000 / 60 );
+		};
+	} )();
+}
 
 var NUM_PARTICLES = ( ( ROWS = 100 ) * ( COLS = 300 ) ),
     THICKNESS = Math.pow( 80, 2 ),
